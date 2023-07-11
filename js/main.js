20ee13c1ce533f8e643f4c057ac53c7b9515b2f5
Jonadesing <109099372+Jonadesing@users.noreply.github.com>
2023-07-11 19:59:51 -0300

    nuevos cambios

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function convertir() {
-    var valore =parseInt(document.getElementById ("valor").value);
-    var resultado = 0;
-    var dolar = 481.25;
-    var euro = 300.89;
+    let valore =parseInt(document.getElementById ("valor").value);
+    let resultado = 0;
+    let dolar = 481.25;
+    let euro = 300.89;
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
-        alert("El cambio de Pesos a Dolares es de:$"+ resultado);
+        alert("El cambio de Pesos a Dolar blue es de:$"+ resultado);
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
-        alert("Elcambio de Pesos a Euros es de:€" + resultado);
+        alert("El cambio de Pesos a Euros oficial es de:€" + resultado);
     }
     else {
         alert("Debes completar todos los campos")
