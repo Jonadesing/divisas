79bfc3a26e05aaded297e4e1580f99004d0dd8f4
Jonadesing <109099372+Jonadesing@users.noreply.github.com>
2023-07-11 19:31:12 -0300

    crear calculador

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function convertir() {
+    var valore =parseInt(document.getElementById ("valor").value);
+    var resultado = 0;
+    var dolar = 481.25;
+    var euro = 300.89;
+    if (document.getElementById("uno").checked) {
+        resultado = valore / dolar;
+        alert("El cambio de Pesos a Dolares es de:"+ resultado);
+    }
+    else if (document.getElementById("dos").checked) {
+        resultado = valore / euro;
+        alert("Elcambio de Pesos a Euros es de:" + resultado);
+    }
+    else {
+        alert("Debes completar todos los campos")
+    }
+}
