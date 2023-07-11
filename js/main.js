b7c0ad1f7726ee378388f72627f98f7062996543
Jonadesing <109099372+Jonadesing@users.noreply.github.com>
2023-07-11 19:36:54 -0300

    correcion

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ function convertir() {
     var euro = 300.89;
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
-        alert("El cambio de Pesos a Dolares es de:"+ resultado);
+        alert("El cambio de Pesos a Dolares es de:$"+ resultado);
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
-        alert("Elcambio de Pesos a Euros es de:" + resultado);
+        alert("Elcambio de Pesos a Euros es de:€" + resultado);
     }
     else {
         alert("Debes completar todos los campos")
