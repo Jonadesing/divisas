82e77d17d9571253db68c2c1235bfdcef16cdfc9
Jonadesing <109099372+Jonadesing@users.noreply.github.com>
2023-07-29 19:41:24 -0300

    agregar metodos, objetos y array

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
-    //============Bienvenida a la pagina!==========
+//============Bienvenida a la pagina!==========
 let nombre = ""
-
 do{nombre = prompt("Ingrese su nombre") } while( ! isNaN(nombre))
+   
+console.log("bienvenidx " + nombre + " a la calculadora de carlitos")
 
-alert("bienvenidx " + nombre + " a la calculadora de carlitos")
+ //==========Función para obtener el día de la semana en español===================
+ function obtenerDiaSemana() {
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const fechaActual = new Date();
+    const diaSemana = diasSemana[fechaActual.getDay()];
+    return diaSemana;
+}
+
+//=============Función para preguntar si desea saber la fecha actual==========
+function preguntarFechaActual() {
+    const respuesta = prompt("¿Deseas saber la fecha actual? (Sí / No)");
+
+    if (respuesta !== null) {
+        const respuestaNormalizada = respuesta.trim().toLowerCase();
+
+        if (respuestaNormalizada === "sí" || respuestaNormalizada === "si") {
+            const diaSemana = obtenerDiaSemana();
+            console.log("Hoy es " + diaSemana);
+        } else {
+            console.log("¡Ok! No te mostraré la fecha actual.");
+        }
+    }
+}
+
+//===================Llamar a la función al cargar la página========
+preguntarFechaActual();
 
 function convertir() {
-    //==================VARIABLES=================
+        //==================VARIABLES=================
     let valore = 0
     let resultado = 0
     let dolar = 545
@@ -16,24 +42,25 @@ function convertir() {
     valore = parseFloat(document.getElementById("valor").value)
     
     if(isNaN(valore)|| valore <= 0){
-        return alert("INGRESE UN NÚMERO, MAYOR A 0 ")
+        return console.log("INGRESE UN NÚMERO, MAYOR A 0 ")
     }
-
-    //==================CAMBIO DOLAR A PESO=================
+        //==================CAMBIO DOLAR A PESO=================
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar
         resultado = resultado.toFixed(2)
-        alert("El cambio de Pesos a Dolar blue es de: $" + resultado)
+        console.log("El cambio de Pesos a Dolar blue es de: $" + resultado)
     }
-
+    
     //==================CAMBIO DOLAR A EURO=================
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
         resultado = resultado.toFixed(2)
-        alert("El cambio de Pesos a Euro oficial es de: €" + resultado)
+        console.log("El cambio de Pesos a Euro oficial es de: €" + resultado);
     }
-
+    
     else {
-        alert("Debes completar todos los campos")
+        console.log("Debes completar todos los campos")
     }
 }
+
+
